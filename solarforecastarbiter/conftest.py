2d05f0b4b0e26f1a2252863ad8737ead2b93a191
Cliff Hansen <cwhanse@sandia.gov>
2019-03-06 08:23:42 -0700

    move tables logic

--- solarforecastarbiter/conftest.py
+++ solarforecastarbiter/conftest.py
@@ -12,14 +12,6 @@ import pytest
 
 from solarforecastarbiter import datamodel
 
-try:
-    import tables
-    has_tables = True
-except ImportError:
-    has_tables = False
-
-requires_tables = pytest.mark.skipif(not has_tables, reason='requires tables')
-
 
 @pytest.fixture(scope='module', params=[
     pd.DataFrame.from_records(
