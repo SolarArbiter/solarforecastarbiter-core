376c8b7c4a5a93b86cb659684a13db23f45c2b45
Cliff Hansen <cwhanse@sandia.gov>
2019-03-06 14:07:28 -0700

    initial day/night function

--- solarforecastarbiter/validation/validator.py
+++ solarforecastarbiter/validation/validator.py
@@ -259,3 +259,36 @@ def check_wind_limits(weather, wind_limits=(0., 60.)):
     flags['extreme_wind_flag'] = _check_limits(wind_speed, lb=wind_limits[0],
                                                ub=wind_limits[1])
     return flags
+
+
+def check_irradiance_day_night(times, solar_position=None, location=None,
+                               max_zenith=87, **kwargs):
+    """ Checks for day/night periods based on solar zenith.
+
+    If solar_position is not provide, location must be provided and solar
+    position will be calculated.
+
+    Parameters
+    ----------
+    times : DatetimeIndex
+    solar_position : None or DataFrame, default None
+        If DataFrame, columns must include ``zenith``.
+    location : None or pvlib.location.Location, default None
+    max_zenith : maximum zenith angle for a daylight time
+    Optional kwargs are passed to the solar position function, if solar
+    position is to be calculated
+
+    Returns
+    -------
+    flags : DataFrame
+        True when solar zenith is greater than max_zenith.
+    """
+    if not solar_position and location is not None:
+        solar_position = get_solarposition(location, times)
+    elif not solar_position and location is None:
+        raise ValueError("Either solar_position or location is required")
+
+    flags = pd.DataFrame(index=times, data=None, columns=['daytime'])
+    flags['daytime'] = _check_limits(solar_position['zenith'], ub=max_zenith)
+    return flags
+
