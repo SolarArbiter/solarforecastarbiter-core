c07a648982622a3baa62c653a349ebeda76b074a
Cliff Hansen <cwhanse@sandia.gov>
2019-03-25 14:20:09 -0600

    Detect stale and interpolated data (#51)
    * level detector
    
    * add linear feature detection
    
    * use atol for linear test
    
    * remove unused import
    
    * fix stale test part 6
    
    * flake8
    
    * use pd.Series.rolling, add near-zero series to tests
    
    * fix linear tests
    
    * update docstring, fix test for <= in np.allclose rather than < which was wrong
    
    * flake8
    
    * remove old

--- solarforecastarbiter/validation/validator.py
+++ solarforecastarbiter/validation/validator.py
@@ -5,10 +5,11 @@ Created on Fri Feb 15 14:08:20 2019
 @author: cwhanse
 """
 
-from pvlib.tools import cosd
-from pvlib.irradiance import clearsky_index
 import numpy as np
 import pandas as pd
+from pvlib.tools import cosd
+from pvlib.irradiance import clearsky_index
+
 
 QCRAD_LIMITS = {'ghi_ub': {'mult': 1.5, 'exp': 1.2, 'min': 100},
                 'dhi_ub': {'mult': 0.95, 'exp': 1.2, 'min': 50},
@@ -406,3 +407,94 @@ def check_timestamp_spacing(times, freq=None):
         return len(gaps) == 1
     else:
         return True  # singleton DatetimeIndex passes
+
+
+def _all_close_to_first(x, rtol=1e-5, atol=1e-8):
+    """ Returns True if all values in x are close to x[0].
+
+    Parameters
+    ----------
+    x : array
+    rtol : float, default 1e-5
+        relative tolerance for detecting a change in data values
+    atol : float, default 1e-8
+        absolute tolerance for detecting a change in data values
+
+    Returns
+    -------
+    Boolean
+    """
+    return np.allclose(a=x, b=x[0], rtol=rtol, atol=atol)
+
+
+def detect_stale_values(x, window=3, rtol=1e-5, atol=1e-8):
+    """ Detects stale data.
+
+    For a window of length N, the last value (index N-1) is considered stale
+    if all values in the window are close to the first value (index 0).
+
+    Parameters
+    ----------
+    x : Series
+        data to be processed
+    window : int, default 3
+        number of consecutive values which, if unchanged, indicates stale data
+    rtol : float, default 1e-5
+        relative tolerance for detecting a change in data values
+    atol : float, default 1e-8
+        absolute tolerance for detecting a change in data values
+
+    Parameters rtol and atol have the same meaning as in numpy.allclose
+
+    Returns
+    -------
+    flags : Series
+        True if the value is part of a stale sequence of data
+    Raises
+    ------
+        ValueError if window < 2
+    """
+    if window < 2:
+        raise ValueError('window set to {}, must be at least 2'.format(window))
+
+    flags = x.rolling(window=window).apply(
+        _all_close_to_first, raw=True, kwargs={'rtol': rtol, 'atol': atol}
+        ).fillna(False).astype(bool)
+    return flags
+
+
+def detect_interpolation(x, window=3, rtol=1e-5, atol=1e-8):
+    """ Detects sequences of data which appear linear.
+
+    Sequences are linear if the first difference appears to be constant.
+    For a window of length N, the last value (index N-1) is flagged
+    if all values in the window appear to be a line segment.
+
+    Parameters
+    ----------
+    x : series
+        data to be processed
+    window : int, default 3
+        number of sequential values that, if the first difference is constant,
+        are classified as a linear sequence
+    rtol : float, default 1e-5
+        tolerance relative to max(abs(x.diff()) for detecting a change
+    atol : float, default 1e-8
+        absolute tolerance for detecting a change in first difference
+
+    Returns
+    -------
+    flags : Series
+        True if the value is part of a linear sequence
+
+    Raises
+    ------
+        ValueError if window < 3
+    """
+    if window < 3:
+        raise ValueError('window set to {}, must be at least 3'.format(window))
+
+    # reduce window by 1 because we're passing the first difference
+    flags = detect_stale_values(x.diff(periods=1), window=window-1, rtol=rtol,
+                                atol=atol)
+    return flags
