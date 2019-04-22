259e040787eb726acf610d997c849eabc6e401ee
David Larson <dplarson@ucsd.edu>
2019-04-22 16:50:10 -0700

    metrics: add (basic) deterministic error metrics
    Add functions to compute the (basic) deterministic error metrics:
    - mean absolute error (MAE)
    - mean bias error (MBE)
    - root mean square error (RMSE)
    
    Note that the function names are likely temporary as we work to decide
    on an optimal balance between clarity and conciseness in naming the
    functions.

--- solarforecastarbiter/metrics/deterministic.py
+++ solarforecastarbiter/metrics/deterministic.py
@@ -1,1 +1,62 @@
 """Deterministic metrics."""
+
+import numpy as np
+
+
+def _mean_absolute(y_true, y_pred):
+    """Mean absolute error (MAE).
+
+    Parameters
+    ----------
+    y_true : array-like
+        True values.
+    y_pred : array-like
+        Predicted values.
+
+    Returns
+    -------
+    mae : float
+        The MAE between the true and predicted values.
+    """
+
+    return np.mean(np.abs(y_true - y_pred))
+
+
+def _mean_bias(y_true, y_pred):
+    """Mean bias error (MBE).
+
+    Parameters
+    ----------
+    y_true : array-like
+        True values.
+    y_pred : array-like
+        Predicted values.
+
+    Returns
+    -------
+    mbe : float
+        The MBE between the true and predicted values.
+
+    """
+
+    return np.mean(y_pred - y_true)
+
+
+def _root_mean_square(y_true, y_pred):
+    """Root mean square error (RMSE).
+
+    Parameters
+    ----------
+    y_true : array-like
+        True values.
+    y_pred : array-like
+        Predicted values.
+
+    Returns
+    -------
+    rmse : float
+        The RMSE between the true and predicted values.
+
+    """
+
+    return np.sqrt(np.mean((y_true - y_pred) ** 2))
