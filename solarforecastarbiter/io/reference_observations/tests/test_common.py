4cf452471b94d7c5e66d9bc10bf91419c362aec0
Leland Boeman <lboeman@email.arizona.edu>
2019-05-03 09:29:44 -0700

    flake8

--- solarforecastarbiter/io/reference_observations/tests/test_common.py
+++ solarforecastarbiter/io/reference_observations/tests/test_common.py
@@ -30,17 +30,18 @@ sites_list = [
         'extra_parameters': '{"network": "NOAA SOLRAD"}',
     }
 ]
-site_objects = [Site.from_dict(site) for site in sites_list] 
+site_objects = [Site.from_dict(site) for site in sites_list]
 
 
 @pytest.fixture()
 def site_dicts():
     return sites_list
 
-    
+
 def test_decode_extra_parameters(site_dicts):
     metadata = Site.from_dict(site_dicts[0])
     assert common.decode_extra_parameters(metadata) == {'network': 'ARM'}
+
 
 @pytest.mark.parametrize('networks,site,expected', [
     (['ARM'], sites_list[0], True),
@@ -51,12 +52,11 @@ def test_check_network(networks, site, expected):
     metadata = Site.from_dict(site)
     assert common.check_network(networks, metadata) == expected
 
+
 @pytest.mark.parametrize('networks,expected', [
     (['ARM'], site_objects[:1]),
-    ('ARM',  site_objects[:1]),
+    ('ARM', site_objects[:1]),
     (['NOAA SURFRAD', 'NOAA SOLRAD'], site_objects[1:])
 ])
 def test_filter_by_network(site_dicts, networks, expected):
-     assert common.filter_by_networks(site_objects, networks) == expected
-     
-
+    assert common.filter_by_networks(site_objects, networks) == expected
