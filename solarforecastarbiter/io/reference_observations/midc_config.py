d9153d9db7b77f22f69c44806df683a4ea04b327
Leland Boeman <lboeman@email.arizona.edu>
2019-05-14 09:21:16 -0700

    flake8

--- solarforecastarbiter/io/reference_observations/midc_config.py
+++ solarforecastarbiter/io/reference_observations/midc_config.py
@@ -15,7 +15,7 @@ midc_var_map = {
         'air_temperature': 'Tower Dry Bulb Temp [deg C]',
         'relative_humidity': 'Tower RH [%]',
     },
-    'UOSMRL':{
+    'UOSMRL': {
         'ghi': 'Global CMP22 [W/m^2]',
         'dni': 'Direct NIP [W/m^2]',
         'dhi': 'Diffuse Schenk [W/m^2]',
@@ -23,18 +23,18 @@ midc_var_map = {
         'relative_humidity': 'Relative Humidity [%]',
         'wind_speed': 'Avg Wind Speed @ 10m [m/s]',
     },
-    'HSU':{
+    'HSU': {
         'ghi': 'Global Horiz [W/m^2]',
         'dni': 'Direct Normal (calc) [W/m^2]',
         'dhi': 'Diffuse Horiz (band_corr) [W/m^2]',
     },
-    'UTPASRL':{
+    'UTPASRL': {
         'ghi': 'Global Horizontal [W/m^2]',
         'dni': 'Direct Normal [W/m^2]',
         'dhi': 'Diffuse Horizontal [W/m^2]',
         'air_temperature': 'CHP1 Temp [deg C]',
     },
-    'UAT':{
+    'UAT': {
         'ghi': 'Global Horiz_(platform) [W/m^2]',
         'dni': 'Direct Normal [W/m^2]',
         'dhi': 'Diffuse Horiz [W/m^2]',
@@ -42,7 +42,7 @@ midc_var_map = {
         'relative_humidity': 'Rel Humidity [%]',
         'wind_speed': 'Avg Wind Speed @ 3m [m/s]',
     },
-    'STAC':{
+    'STAC': {
         'ghi': 'Global Horizontal [W/m^2]',
         'dni': 'Direct Normal [W/m^2]',
         'dhi': 'Diffuse Horizontal [W/m^2]',
@@ -50,14 +50,14 @@ midc_var_map = {
         'air_temperature': 'Air Temperature [deg C]',
         'relative_humidity': 'Rel Humidity [%]',
     },
-    'UNLV':{
+    'UNLV': {
         'ghi': 'Global Horiz [W/m^2]',
         'dni': 'Direct Normal [W/m^2]',
         'dhi': 'Diffuse Horiz (calc) [W/m^2]',
         'air_temperature': 'Dry Bulb Temp [deg C]',
         'wind_speed': 'Avg Wind Speed @ 30ft [m/s]',
     },
-    'ORNL':{
+    'ORNL': {
         'ghi': 'Global Horizontal [W/m^2]',
         'dni': 'Direct Normal [W/m^2]',
         'dhi': 'Diffuse Horizontal [W/m^2]',
@@ -65,13 +65,13 @@ midc_var_map = {
         'relative_humidity': 'Rel Humidity [%]',
         'wind_speed': 'Avg Wind Speed @ 42ft [m/s]',
     },
-    'NELHA':{
+    'NELHA': {
         'ghi': 'Global Horizontal [W/m^2]',
         'air_temperature': 'Air Temperature [W/m^2]',
         'wind_speed': 'Avg Wind Speed @ 10m [m/s]',
         'relative_humidity': 'Rel Humidity [%]',
     },
-    'ULL':{
+    'ULL': {
         'ghi': 'Global Horizontal [W/m^2]',
         'dni': 'Direct Normal [W/m^2]',
         'dhi': 'Diffuse Horizontal [W/m^2]',
@@ -79,7 +79,7 @@ midc_var_map = {
         'relative_humidity': 'Rel Humidity [%]',
         'wind_speed': 'Avg Wind Speed @ 3m [m/s]',
     },
-    'VTIF':{
+    'VTIF': {
         'ghi': 'Global Horizontal [W/m^2]',
         'dni': 'Direct Normal [W/m^2]',
         'dhi': 'Diffuse Horizontal [W/m^2]',
@@ -87,7 +87,7 @@ midc_var_map = {
         'wind_speed': 'Avg Wind Speed @ 3m [m/s]',
         'relative_humidity': 'Rel Humidity [%]'
     },
-    'NWTC':{
+    'NWTC': {
         'ghi': 'Global PSP [W/m^2]',
         'air_temperature': 'Temperature @ 2m [deg C]',
         'wind_speed': 'Avg Wind Speed @ 2m [m/s]',
