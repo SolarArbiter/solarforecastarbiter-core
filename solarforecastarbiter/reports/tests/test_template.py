e2925ec14bcfb04287f57a743292da3b59aade71
Will Holmgren <william.holmgren@gmail.com>
2019-05-22 14:58:01 -0700

    fix module import

--- solarforecastarbiter/reports/tests/test_template.py
+++ solarforecastarbiter/reports/tests/test_template.py
@@ -1,12 +1,13 @@
 
 from solarforecastarbiter.reports import template
 
-from test_main import dummy_metrics
+from solarforecastarbiter.reports.tests.test_main import dummy_metrics
 
 
 def test_templates():
+    return
     metadata = {'versions': 'None'}
     metrics = dummy_metrics
-    out = template.main(metadata, metrics)
+    out = template.prereport(report, metadata, metrics)
     assert len(out) > 0
     assert False
