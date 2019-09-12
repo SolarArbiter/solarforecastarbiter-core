26d71295f0437890bd6c4f0300b470a276702c79
David Larson <dplarson@ucsd.edu>
2019-09-12 14:25:16 -0700

    metrics/tests: add tests for NRMSE and forecast skill

--- solarforecastarbiter/metrics/tests/test_deterministic.py
+++ solarforecastarbiter/metrics/tests/test_deterministic.py
@@ -20,7 +20,7 @@ def test_mbe(y_true, y_pred, value):
 
 
 @pytest.mark.parametrize("y_true,y_pred,value", [
-    (np.array([0, 1, 2]), np.array([0, 1, 2]), 0.0),
+    (np.array([0, 1]), np.array([0, 1]), 0.0),
     (np.array([0, 1]), np.array([1, 2]), 1.0),
 ])
 def test_rmse(y_true, y_pred, value):
@@ -33,3 +33,26 @@ def test_rmse(y_true, y_pred, value):
 ])
 def test_mape(y_true, y_pred, value):
     assert deterministic.mean_absolute_percentage(y_true, y_pred) == value
+
+
+@pytest.mark.parametrize("y_true,y_pred,y_norm,value", [
+    (np.array([0, 1, 2]), np.array([0, 1, 2]), 1.0, 0.0),
+    (np.array([0, 1, 2]), np.array([0, 1, 2]), 55.0, 0.0),
+    (np.array([0, 1]), np.array([1, 2]), 1.0, 100.0),
+    (np.array([0, 1]), np.array([1, 2]), 100.0, 1.0),
+])
+def test_nrmse(y_true, y_pred, y_norm, value):
+    assert deterministic.normalized_root_mean_square(y_true, y_pred, y_norm) == value
+
+@pytest.mark.parametrize("y_true,y_pred,y_ref,value", [
+    (np.array([0, 1]), np.array([0, 2]), np.array([0, 2]), 1.0 - 1.0 / 1.0),
+    (np.array([0, 1]), np.array([0, 2]), np.array([0, 3]), 1.0 - 1.0 / 2.0),
+])
+def test_skill(y_true, y_pred, y_ref, value):
+    assert deterministic.forecast_skill(y_true, y_pred, y_ref) == value
+
+# Pearson corr coeff (r) [-]
+
+# Coeff of deterministation (R^2) [-]
+
+# CRMSE
