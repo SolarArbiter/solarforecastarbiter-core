e677438abc2cae1cee1d86d2e5a5aa7dca974e5f
David Larson <dplarson@ucsd.edu>
2019-09-12 14:37:00 -0700

    metrics/tests: add R^2 and CRMSE tests

--- solarforecastarbiter/metrics/tests/test_deterministic.py
+++ solarforecastarbiter/metrics/tests/test_deterministic.py
@@ -8,7 +8,8 @@ from solarforecastarbiter.metrics import deterministic
     (np.array([0, 1, 2]), np.array([0, 1, 1]), 1 / 3),
 ])
 def test_mae(y_true, y_pred, value):
-    assert deterministic.mean_absolute(y_true, y_pred) == value
+    mae = deterministic.mean_absolute(y_true, y_pred)
+    assert mae == value
 
 
 @pytest.mark.parametrize("y_true,y_pred,value", [
@@ -16,7 +17,8 @@ def test_mae(y_true, y_pred, value):
     (np.array([0, 1, 2]), np.array([1, 0, 2]), 0.0),
 ])
 def test_mbe(y_true, y_pred, value):
-    assert deterministic.mean_bias(y_true, y_pred) == value
+    mbe = deterministic.mean_bias(y_true, y_pred)
+    assert mbe == value
 
 
 @pytest.mark.parametrize("y_true,y_pred,value", [
@@ -24,7 +26,8 @@ def test_mbe(y_true, y_pred, value):
     (np.array([0, 1]), np.array([1, 2]), 1.0),
 ])
 def test_rmse(y_true, y_pred, value):
-    assert deterministic.root_mean_square(y_true, y_pred) == value
+    rmse = deterministic.root_mean_square(y_true, y_pred)
+    assert rmse == value
 
 
 @pytest.mark.parametrize("y_true,y_pred,value", [
@@ -32,7 +35,8 @@ def test_rmse(y_true, y_pred, value):
     (np.array([2, 2]), np.array([3, 3]), 50.0),
 ])
 def test_mape(y_true, y_pred, value):
-    assert deterministic.mean_absolute_percentage(y_true, y_pred) == value
+    mape = deterministic.mean_absolute_percentage(y_true, y_pred)
+    assert mape == value
 
 
 @pytest.mark.parametrize("y_true,y_pred,y_norm,value", [
@@ -42,17 +46,39 @@ def test_mape(y_true, y_pred, value):
     (np.array([0, 1]), np.array([1, 2]), 100.0, 1.0),
 ])
 def test_nrmse(y_true, y_pred, y_norm, value):
-    assert deterministic.normalized_root_mean_square(y_true, y_pred, y_norm) == value
+    nrmse = deterministic.normalized_root_mean_square(y_true, y_pred, y_norm)
+    assert nrmse == value
+
 
 @pytest.mark.parametrize("y_true,y_pred,y_ref,value", [
     (np.array([0, 1]), np.array([0, 2]), np.array([0, 2]), 1.0 - 1.0 / 1.0),
     (np.array([0, 1]), np.array([0, 2]), np.array([0, 3]), 1.0 - 1.0 / 2.0),
 ])
 def test_skill(y_true, y_pred, y_ref, value):
-    assert deterministic.forecast_skill(y_true, y_pred, y_ref) == value
+    s = deterministic.forecast_skill(y_true, y_pred, y_ref)
+    assert s == value
 
-# Pearson corr coeff (r) [-]
 
-# Coeff of deterministation (R^2) [-]
+@pytest.mark.parametrize("y_true,y_pred,value", [
+    (np.array([0, 1]), np.array([0, 1]), 1.0),
+    (np.array([1, 2]), np.array([-1, -2]), -1.0),
+])
+def test_r(y_true, y_pred, value):
+    r = deterministic.pearson_correlation_coeff(y_true, y_pred)
+    assert pytest.approx(r) == value
 
-# CRMSE
+
+@pytest.mark.parametrize("y_true,y_pred,value", [
+    (np.array([0, 1]), np.array([0, 1]), 1.0),
+])
+def test_r2(y_true, y_pred, value):
+    r2 = deterministic.coeff_determination(y_true, y_pred)
+    assert pytest.approx(r2) == value
+
+
+@pytest.mark.parametrize("y_true,y_pred,value", [
+    (np.array([0, 1]), np.array([0, 1]), 0.0),
+])
+def test_crmse(y_true, y_pred, value):
+    crmse = deterministic.centered_root_mean_square(y_true, y_pred)
+    assert crmse == value
