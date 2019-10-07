a9a4cce76c50d6d224fd96659d76d668eceddbdc
David Larson <dplarson@ucsd.edu>
2019-10-07 11:14:23 -0700

    metrics: add pd.Series tests for prob metrics

--- solarforecastarbiter/metrics/tests/test_probabilistic.py
+++ solarforecastarbiter/metrics/tests/test_probabilistic.py
@@ -1,5 +1,6 @@
 import pytest
 import numpy as np
+import pandas as pd
 from solarforecastarbiter.metrics import probabilistic as prob
 
 
@@ -20,6 +21,14 @@ def test_brier_score_scalar(fx, obs, value):
     (np.array([1.0, 1.0]), np.array([0, 0]), 1.0),
 ])
 def test_brier_score_vector(fx, obs, value):
+    assert prob.brier_score(fx, obs) == value
+
+
+@pytest.mark.parametrize("fx,obs,value", [
+    (pd.Series([1.0, 1.0]), pd.Series([1, 1]), 0.0),
+    (pd.Series([1.0, 1.0]), pd.Series([0, 0]), 1.0),
+])
+def test_brier_score_series(fx, obs, value):
     assert prob.brier_score(fx, obs) == value
 
 
@@ -49,6 +58,24 @@ def test_brier_skill_score_vector(fx, obs, ref, value):
     assert prob.brier_skill_score(fx, obs, ref) == value
 
 
+@pytest.mark.parametrize("fx,obs,ref,value", [
+    (
+        pd.Series([1.0, 1.0]),
+        pd.Series([0, 0]),
+        pd.Series([1.0, 1.0]),
+        1.0 - 1.0 / 1.0
+    ),
+    (
+        pd.Series([1.0, 1.0]),
+        pd.Series([0, 0]),
+        pd.Series([0.5, 0.5]),
+        1.0 - 1.0 / 0.25
+    ),
+])
+def test_brier_skill_score_series(fx, obs, ref, value):
+    assert prob.brier_skill_score(fx, obs, ref) == value
+
+
 @pytest.mark.parametrize("fx_lower,fx_upper,value", [
     (0.0, 1.0, 1.0),
     (0.5, 0.6, 0.1),
@@ -63,5 +90,14 @@ def test_sharpness_scalar(fx_lower, fx_upper, value):
     (np.array([0.2, 0.7]), np.array([0.3, 0.8]), 0.1),
 ])
 def test_sharpness_vector(fx_lower, fx_upper, value):
+    sh = prob.sharpness(fx_lower, fx_upper)
+    assert pytest.approx(sh) == value
+
+
+@pytest.mark.parametrize("fx_lower,fx_upper,value", [
+    (pd.Series([0.0, 0.0]), pd.Series([1.0, 1.0]), 1.0),
+    (pd.Series([0.2, 0.7]), pd.Series([0.3, 0.8]), 0.1),
+])
+def test_sharpness_series(fx_lower, fx_upper, value):
     sh = prob.sharpness(fx_lower, fx_upper)
     assert pytest.approx(sh) == value
