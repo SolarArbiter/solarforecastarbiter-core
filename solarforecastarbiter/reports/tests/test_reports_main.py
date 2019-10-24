1283ec335b24711d860fe38cce3f1a263faddf67
Adam Wigington <ajwigington@gmail.com>
2019-10-23 22:20:25 -0700

    Added another render test to test_reports_main to test all categories

--- solarforecastarbiter/reports/tests/test_reports_main.py
+++ solarforecastarbiter/reports/tests/test_reports_main.py
@@ -9,7 +9,7 @@ import pytest
 from solarforecastarbiter import datamodel
 from solarforecastarbiter.io import api
 from solarforecastarbiter.reports import template, main
-
+from solarforecastarbiter.metrics import calculator
 
 @pytest.fixture(scope='module')
 def _test_data(report_objects):
@@ -63,6 +63,30 @@ def test_full_render(mock_data, report_objects):
     session = api.APISession('nope')
     data = main.get_data_for_report(session, report)
     raw_report = main.create_raw_report_from_data(report, data)
+    report_md = main.render_raw_report(raw_report)
+    body = template.report_md_to_html(report_md)
+    full_report = template.full_html(body)
+    with open('bokeh_report.html', 'w') as f:
+        f.write(full_report)
+
+
+@pytest.mark.skipif(shutil.which('pandoc') is None,
+                    reason='Pandoc can not be found')
+def test_all_categories_render(mock_data, report_objects):
+    report, observation, forecast_0, forecast_1 = report_objects
+    all_report = datamodel.Report(
+        name=report.name,
+        start=report.start,
+        end=report.end,
+        forecast_observations=report.forecast_observations,
+        metrics=("mae", "rmse", "mbe"),
+        categories=["total", "day", 'hour'],
+        report_id=report.report_id,
+        filters=report.filters
+    )
+    session = api.APISession('nope')
+    data = main.get_data_for_report(session, all_report)
+    raw_report = main.create_raw_report_from_data(all_report, data)
     report_md = main.render_raw_report(raw_report)
     body = template.report_md_to_html(report_md)
     full_report = template.full_html(body)
