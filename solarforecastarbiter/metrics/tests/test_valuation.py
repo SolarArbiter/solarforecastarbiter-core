c99cf49b64121508d0e123dbd7a33c68f9a4befb
David Larson <dplarson@ucsd.edu>
2019-11-21 17:00:58 -0800

    metrics: valuation: expand tests

--- solarforecastarbiter/metrics/tests/test_valuation.py
+++ solarforecastarbiter/metrics/tests/test_valuation.py
@@ -4,9 +4,12 @@ from solarforecastarbiter.metrics import valuation
 
 
 @pytest.mark.parametrize("fx,obs,cost,value", [
-    # 10 USD per MW
+    # cost: 10 USD per kW
     (np.array([1, 2, 3]), np.array([1, 2, 3]), 10, 0.0),
     (np.array([1, 2, 3]), np.array([2, 3, 4]), 10, 3 * 10),
+
+    # cost: 1 USD per W/m^2
+    (np.array([500, 600, 650]), np.array([500, 580, 630]), 1, 20 + 20),
 ])
 def test_fixed_cost(fx, obs, cost, value):
     return valuation.fixed_error_cost(fx, obs, cost) == value
