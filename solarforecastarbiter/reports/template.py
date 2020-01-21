28a0f797abe34571aec32e6ee2215305062f5429
Leland Boeman <lboeman@email.arizona.edu>
2020-01-21 11:54:55 -0700

    Docstrings (#307)
    * add reports template.py testing
    
    * update figures, template docstring
    
    * add parameters to non-private datamodel objects, add sphinx decorators
    
    * add reports modules to docs
    
    * add reports api changes to whatsnew
    
    * flake8
    
    * correct docstrings
    
    * remove data_object from ForecastAggregate docstring
    
    Co-authored-by: Tony Lorenzo <atlorenzo@email.arizona.edu>

--- solarforecastarbiter/reports/template.py
+++ solarforecastarbiter/reports/template.py
@@ -15,6 +15,25 @@ logger = logging.getLogger(__name__)
 
 
 def _get_render_kwargs(report, dash_url, with_timeseries):
+    """Creates a dictionary of key word template arguments for a jinja2
+    report template.
+
+    Parameters
+    ----------
+    report: :py:class:`solarforecastarbiter.datamodel.Report`
+    dash_url: str
+        URL of the Solar Forecast arbiter dashboard to use when building links.
+    with_timeseries: bool
+        Whether or not to include timeseries plots. If an error occurs, sets
+        `timeseries_script` to an empty string and `timeseries_div` will
+        contain a <div> element for warning the user of the failure.
+
+    Returns
+    -------
+    kwargs: dict
+        Dictionary of template variables to unpack as key word arguments when
+        rendering.
+    """
     kwargs = dict(
         human_categories=datamodel.ALLOWED_CATEGORIES,
         human_metrics=datamodel.ALLOWED_METRICS,
@@ -39,6 +58,28 @@ def _get_render_kwargs(report, dash_url, with_timeseries):
 
 
 def get_template_and_kwargs(report, dash_url, with_timeseries, body_only):
+    """Returns the jinja2 Template object and a dict of template variables for
+    the report.
+
+    Parameters
+    ----------
+    report: :py:class:`solarforecastarbiter.datamodel.Report`
+    dash_url: str
+        URL of the Solar Forecast arbiter dashboard to use when building links.
+    with_timeseries: bool
+        Whether or not to include timeseries plots.
+    body_only: bool
+        When True, returns a div for injecting into another template,
+        otherwise returns a standalone report template with the required
+        <html> and <head> tags.
+
+    Returns
+    -------
+    template: jinja2.environment.Template
+    kwargs: dict
+        Dictionary of template variables to use as keyword arguments to
+        template.render().
+    """
     env = Environment(
         loader=PackageLoader('solarforecastarbiter.reports', 'templates'),
         autoescape=select_autoescape(['html', 'xml']),
@@ -62,12 +103,20 @@ def render_html(report, dash_url='https://dashboard.solarforecastarbiter.org',
 
     Parameters
     ----------
-    body : html
+    report: :py:class:`solarforecastarbiter.datamodel.Report`
+    dash_url: str
+        URL of the Solar Forecast arbiter dashboard to use when building links.
+    with_timeseries: bool
+        Whether or not to include timeseries plots.
+    body_only: bool
+        When True, returns a div for injecting into another template,
+        otherwise returns a standalone report template with the required
+        <html> and <head> tags.
 
     Returns
     -------
-        head : str, html
-        Header for the full report.
+    str
+        The rendered html report
     """
     template, kwargs = get_template_and_kwargs(
         report, dash_url, with_timeseries, body_only)
