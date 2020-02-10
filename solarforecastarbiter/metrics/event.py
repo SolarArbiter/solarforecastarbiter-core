14ba4655df72bd8a33781ee0c1cf29c244ccd88b
David P. Larson <dlarson@epri.com>
2020-02-10 12:02:49 -0800

    metrics: outline event metric functions and tests

---
+++ solarforecastarbiter/metrics/event.py
@@ -0,0 +1,33 @@
+"""Event forecast error metrics."""
+
+import numpy as np
+
+
+def probability_of_detection(obs, fx):
+    """Probability of Detection (POD)."""
+    pass
+
+
+def false_alarm_ratio(obs, fx):
+    """False Alarm Ratio (FAR)."""
+    pass
+
+
+def probability_of_false_detection(obs, fx):
+    """Probability of False Detection (POFD)."""
+    pass
+
+
+def critical_success_index(obs, fx):
+    """Critical Success Index (CSI)."""
+    pass
+
+
+def event_bias(obs, fx):
+    """Event Bias (EBIAS)."""
+    pass
+
+
+def event_accuracy(obs, fx):
+    """Event Accuracy (EA)."""
+    pass
---
+++ solarforecastarbiter/metrics/tests/test_event.py
@@ -0,0 +1,27 @@
+import pytest
+import numpy as np
+from solarforecastarbiter.metrics import event
+
+
+def test_probability_of_detection():
+    pass
+
+
+def test_false_alarm_ratio():
+    pass
+
+
+def test_probability_of_false_detection():
+    pass
+
+
+def test_critical_success_index():
+    pass
+
+
+def test_event_bias():
+    pass
+
+
+def test_event_accuracy():
+    pass
