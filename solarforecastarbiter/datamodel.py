fix normalization metrics in reports (#379)
* 90 percent done, 90 percent to go

* fix dc, add issue link

* fix tests

* i hate tests

* well, mostly the datamodel objects

* doc for normalization

* nan normalization

* minor name change

* flake8

* remove unneeded try/except

solarforecastarbiter/datamodel.py
@@ -13,6 +13,7 @@ from jsonschema.exceptions import ValidationError
 from typing import Tuple, Union
 
 
+import numpy as np
 import pandas as pd
 
 
@@ -914,23 +915,49 @@ class ForecastObservation(BaseModel):
     forecast: :py:class:`solarforecastarbiter.datamodel.Forecast`
     observation: :py:class:`solarforecastarbiter.datamodel.Observation`
     reference_forecast: :py:class:`solarforecastarbiter.datamodel.Forecast` or None
-    normalization: float
+    normalization: float or None
+        If None, determined by __set_normalization__
     cost_per_unit_error: float
     """  # NOQA
     forecast: Forecast
     observation: Observation
     reference_forecast: Union[Forecast, None] = None
-    # for now, some function applied to observation (e.g. mean per day)
-    # possible in future
-    normalization: float = 1.0
+    # some function applied to observation (e.g. mean per day)
+    # possible in future. maybe add pd.Series like for
+    # ProcessedForecastObservation
+    normalization: Union[float, None] = None
     # cost: Cost
     cost_per_unit_error: float = 0.0
     data_object: Observation = field(init=False)
 
     def __post_init__(self):
+        if self.normalization is None:
+            __set_normalization__(self)
         object.__setattr__(self, 'data_object', self.observation)
         __check_units__(self.forecast, self.data_object)
         __check_interval_compatibility__(self.forecast, self.data_object)
+
+
+def __set_normalization__(self):
+    if self.observation.variable == 'ac_power':
+        norm = self.observation.site.modeling_parameters.ac_capacity
+    elif self.observation.variable == 'dc_power':
+        norm = self.observation.site.modeling_parameters.dc_capacity
+    elif self.observation.units == 'W/m^2':
+        # normalizing by 1000 W/m^2 was considered and rejected
+        # https://github.com/SolarArbiter/solarforecastarbiter-core/pull/379#discussion_r402434134
+        # keep W/m^2 as separate item for likely future improvements
+        norm = np.nan
+    else:
+        norm = np.nan
+    norm = float(norm)  # from_dict only checks for floats, chokes on ints
+    object.__setattr__(self, 'normalization', norm)
+
+
+def __set_aggregate_normalization__(self):
+    # https://github.com/SolarArbiter/solarforecastarbiter-core/issues/381
+    norm = np.nan
+    object.__setattr__(self, 'normalization', norm)
 
 
 @dataclass(frozen=True)
@@ -943,17 +970,20 @@ class ForecastAggregate(BaseModel):
     forecast: :py:class:`solarforecastarbiter.datamodel.Forecast`
     aggregate: :py:class:`solarforecastarbiter.datamodel.Aggregate`
     reference_forecast: :py:class:`solarforecastarbiter.datamodel.Forecast` or None
-    normalization: float
+    normalization: float or None
+        If None, assigned 1.
     cost_per_unit_error: float
     """  # NOQA
     forecast: Forecast
     aggregate: Aggregate
     reference_forecast: Union[Forecast, None] = None
-    normalization: float = 1.0
+    normalization: Union[float, None] = None
     cost_per_unit_error: float = 0.0
     data_object: Aggregate = field(init=False)
 
     def __post_init__(self):
+        if self.normalization is None:
+            __set_aggregate_normalization__(self)
         object.__setattr__(self, 'data_object', self.aggregate)
         __check_units__(self.forecast, self.data_object)
         __check_interval_compatibility__(self.forecast, self.data_object)

solarforecastarbiter/metrics/tests/test_calculator.py
@@ -214,16 +214,13 @@ def test_calculate_deterministic_metrics_normalizer(
         create_processed_fxobs, single_forecast_observation):
     pair = create_processed_fxobs(single_forecast_observation,
                                   np.random.randn(10), np.random.randn(10))
-    s0 = calculator.calculate_deterministic_metrics(
-        pair, ['total'], deterministic._REQ_NORM, normalizer=1.0)
-    s1 = calculator.calculate_deterministic_metrics(
-        pair, ['total'], deterministic._REQ_NORM, normalizer=1.0)
-    s2 = calculator.calculate_deterministic_metrics(
-        pair, ['total'], deterministic._REQ_NORM, normalizer=2.0)
-    for s in [s0, s1, s2]:
-        assert isinstance(s, datamodel.MetricResult)
-    assert s0 == s1
-    assert s1 != s2
+    s_normed = calculator.calculate_deterministic_metrics(
+        pair, ['total'], deterministic._REQ_NORM)
+    unnormed = [x.lstrip('n') for x in deterministic._REQ_NORM]
+    s_unnormed = calculator.calculate_deterministic_metrics(
+        pair, ['total'], unnormed)
+    for v_normed, v_unnormed in zip(s_normed.values, s_unnormed.values):
+        assert v_normed.value == v_unnormed.value * 100
 
 
 def test_calculate_deterministic_metrics_reference(
@@ -335,7 +332,7 @@ def test_apply_deterministic_metric_func(metric, fx, obs, ref_fx, norm, expect,
                           dtype=float)
     obs_series = pd.Series(obs, index=create_datetime_index(len(obs)),
                            dtype=float)
-    # Check require reference forecast kwarg
+    # Check require reference forecast kwarg (based on parametrize args)
     if metric in ['s']:
         if ref_fx is None:
             with pytest.raises(KeyError):
@@ -358,7 +355,7 @@ def test_apply_deterministic_metric_func(metric, fx, obs, ref_fx, norm, expect,
                     metric, fx_series, obs_series)
         else:
             metric_value = calculator._apply_deterministic_metric_func(
-                metric, fx_series, obs_series, normalizer=norm)
+                metric, fx_series, obs_series, normalization=norm)
             np.testing.assert_approx_equal(metric_value, expect)
 
     # Does not require kwarg

solarforecastarbiter/tests/test_datamodel.py
@@ -3,6 +3,7 @@ import json
 from typing import Union
 
 
+import numpy as np
 import pandas as pd
 import pytest
 
@@ -198,7 +199,14 @@ def test_from_dict_into_datamodel_no_extra(pdid_params):
     for field in fields(model):
         if field.name == 'extra_parameters':
             continue
-        assert getattr(out, field.name) == getattr(expected, field.name)
+        expected_value = getattr(expected, field.name)
+        out_value = getattr(out, field.name)
+        # normalization can be nan. isinstance ensures that isnan doesn't
+        # try to coerce random strings or objects to a float and then error
+        if isinstance(expected_value, float) and np.isnan(expected_value):
+            assert np.isnan(out_value)
+        else:
+            assert out_value == expected_value
 
 
 def test_from_dict_no_extra(pdid_params):
@@ -480,7 +488,7 @@ def test___check_metrics__probabilistic(metrics, prob_forecast_constant_value):
 
 @pytest.fixture
 def objects_from_attrs(mocker):
-    """Takes a list of lists with tupples of (attr_name, value)
+    """Takes a list of lists with tuples of (attr_name, value)
        and creates a listed of Mock objects with their attributes
        set to those values
     """
@@ -731,3 +739,16 @@ def test_report_figure_from_dict_with_plotly(plotly_report_figure_dict):
 def test_report_figure_from_dict_invalid(fig_dict):
     with pytest.raises(NotImplementedError):
         datamodel.ReportFigure.from_dict(fig_dict)
+
+
+def test_ForecastObservation_normalization_ac(single_forecast_ac_observation):
+    assert single_forecast_ac_observation.normalization == 0.003
+
+
+def test_ForecastObservation_normalization_dc(single_forecast_dc_observation):
+    assert single_forecast_dc_observation.normalization == 0.0035
+
+
+def test_ForecastObservation_normalization_wind_speed(
+        single_forecast_wind_speed_observation):
+    assert np.isnan(single_forecast_wind_speed_observation.normalization)