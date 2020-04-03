9b7dbd45dd3b8d16c33b9935dd1295318b056c92
Will Holmgren <william.holmgren@gmail.com>
2020-04-03 09:58:52 -0700

    i hate tests

--- solarforecastarbiter/tests/test_datamodel.py
+++ solarforecastarbiter/tests/test_datamodel.py
@@ -480,7 +480,7 @@ def test___check_metrics__probabilistic(metrics, prob_forecast_constant_value):
 
 @pytest.fixture
 def objects_from_attrs(mocker):
-    """Takes a list of lists with tupples of (attr_name, value)
+    """Takes a list of lists with tuples of (attr_name, value)
        and creates a listed of Mock objects with their attributes
        set to those values
     """
@@ -731,3 +731,16 @@ def test_report_figure_from_dict_with_plotly(plotly_report_figure_dict):
 def test_report_figure_from_dict_invalid(fig_dict):
     with pytest.raises(NotImplementedError):
         datamodel.ReportFigure.from_dict(fig_dict)
+
+
+def test_ForecastObservation_normalization_ac(single_forecast_ac_observation):
+    assert single_forecast_ac_observation.normalization == 0.003
+
+
+def test_ForecastObservation_normalization_dc(single_forecast_dc_observation):
+    assert single_forecast_dc_observation.normalization == 0.0035
+
+
+def test_ForecastObservation_normalization_wind_speed(
+        single_forecast_wind_speed_observation):
+    assert single_forecast_wind_speed_observation.normalization == 1.
