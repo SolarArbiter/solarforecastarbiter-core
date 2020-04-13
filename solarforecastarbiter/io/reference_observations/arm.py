7aeeae247d6ebe217a1f0089e12eb0feb12a5d99
Leland Boeman <lboeman@email.arizona.edu>
2020-04-13 16:25:17 -0700

    arm data parsing code

--- solarforecastarbiter/io/reference_observations/arm.py
+++ solarforecastarbiter/io/reference_observations/arm.py
@@ -15,38 +15,47 @@ from solarforecastarbiter.io.reference_observations import (
 logger = logging.getLogger('reference_data')
 
 
-DOE_RTC_VARIABLE_MAP = {
-    'GlobalIrrad': 'ghi',
-    'DiffuseIrrad': 'dhi',
-    'DirectIrrad': 'dni',
-    'AmbientTemp_weather': 'air_temperature',
-    'WindSpeed': 'wind_speed',
-    'RelativeHumidity': 'relative_humidity',
-    'PyranometerIrrad': 'poa_global',
-    'Sys1Wac': 'ac_power'
+# ARM data streams include 'met' for meteorological sites and 'qcrad' for
+# irradiance data.
+DOE_ARM_SITE_VARIABLES = {
+    'qcrad': arm.IRRAD_VARIABLES,
+    'met': arm.MET_VARIABLES,
+}
+
+DOE_ARM_VARIABLE_MAP = {
+    'down_short_hemisp': 'ghi',
+    'short_direct_normal': 'dni',
+    'down_short_diffuse_hemisp': 'dhi',
+    'temp_mean': 'air_temperature',
+    'rh_mean': 'relative_humidity',
+    'wspd_arith_mean': 'wind_speed',
 }
 
 
-def adjust_site_parameters(site):
-    """Add the PV modeling parameters"""
-    out = site.copy()
-    modeling_params = {
-        'ac_capacity': 0.00324,  # no clipping
-        'dc_capacity': 0.00324,
-        'temperature_coefficient': -0.00420,
-        'dc_loss_factor': 0,
-        'ac_loss_factor': 0,
-        'surface_tilt': 35,
-        'surface_azimuth': 180,
-        'tracking_type': 'fixed'}
-    out['modeling_parameters'] = modeling_params
-    out['extra_parameters']['module'] = 'Suniva 270W'
-    return out
+def _determine_site_vars(datastream):
+    """Returns a list of variables available based on datastream name.
+
+    Parameters
+    ----------
+    datastream: str
+        datastream field of the site. This should be found in the
+        `extra_parameters` field as `network_api_id`
+
+    Returns
+    -------
+    list of str
+        The variable names that can be found in the file.
+    """
+    available = []
+    for stream_type, arm_vars in DOE_ARM_SITE_VARIABLES.items():
+        if stream_type in datastream:
+            available = available + arm_vars
+    return available
 
 
 def initialize_site_observations(api, site):
     """Creates an observation at the site for each variable in
-    DOE_RTC_VARIABLE_MAP
+    the matched DOE_ARM_VARIABLE_MAP.
 
     Parameters
     ----------
@@ -55,20 +64,28 @@ def initialize_site_observations(api, site):
     site : datamodel.Site
         The site object for which to create Observations.
     """
-    for sfa_var in DOE_RTC_VARIABLE_MAP.values():
+    try:
+        site_extra_params = common.decode_extra_parameters(site)
+    except ValueError:
+        logger.error(f'Failed to initialize observations  for {site.name} '
+                     'extra parameters could not be loaded.')
+        return
+    site_arm_vars = _determine_site_vars(site_extra_params['network_api_id'])
+    site_sfa_vars = [DOE_ARM_VARIABLE_MAP[v] for v in site_arm_vars]
+    for sfa_var in site_sfa_vars:
         logger.info(f'Creating {sfa_var} at {site.name}')
         try:
             common.create_observation(
                 api, site, sfa_var)
         except HTTPError as e:
             logger.error(f'Could not create Observation for "{sfa_var}" '
-                         f'at DOE RTC site {site.name}')
+                         f'at DOE ARM site {site.name}')
             logger.debug(f'Error: {e.response.text}')
 
 
 def initialize_site_forecasts(api, site):
     """
-    Create a forecasts for each variable in DOE_RTC_VARIABLE_MAP at each site
+    Create a forecast for each variable at the site.
 
     Parameters
     ----------
@@ -77,12 +94,19 @@ def initialize_site_forecasts(api, site):
     site : datamodel.Site
         The site object for which to create Forecasts.
     """
-    common.create_forecasts(api, site, DOE_RTC_VARIABLE_MAP.values(),
+    try:
+        site_extra_params = common.decode_extra_parameters(site)
+    except ValueError:
+        logger.error('Failed to initialize reference forecasts for '
+                     f'{site.name} extra parameters could not be loaded.')
+        return
+    site_vars = _determine_site_vars(site_extra_params['network_api_id'])
+    common.create_forecasts(api, site, site_vars,
                             default_forecasts.TEMPLATE_FORECASTS)
 
 
-def fetch(api, site, start, end, *, doe_rtc_api_key):
-    """Retrieve observation data for a DOE RTC site between start and end.
+def fetch(api, site, start, end, *, doe_arm_user_id, doe_arm_api_key):
+    """Retrieve observation data for a DOE ARM site between start and end.
 
     Parameters
     ----------
@@ -94,8 +118,10 @@ def fetch(api, site, start, end, *, doe_rtc_api_key):
         The beginning of the period to request data for.
     end : datetime
         The end of the period to request data for.
-    doe_rtc_api_key : str
-        API key to access the DOE RTC api
+    doe_arm_user_id : str
+        User ID to access the DOE ARM api.
+    doe_arm_api_key : str
+        API key to access the DOE ARM api.
 
     Returns
     -------
@@ -106,24 +132,22 @@ def fetch(api, site, start, end, *, doe_rtc_api_key):
         site_extra_params = common.decode_extra_parameters(site)
     except ValueError:
         return pd.DataFrame()
-    doe_rtc_site_id = site_extra_params['network_api_id']
-    obs_df = rtc.fetch_doe_rtc(
-        doe_rtc_site_id, doe_rtc_api_key,
+    doe_arm_datastream = site_extra_params['network_api_id']
+    site_vars = _determine_site_vars(doe_arm_datastream)
+    obs_df = arm.fetch_arm(
+        doe_arm_user_id, doe_arm_api_key, doe_arm_datastream, site_vars,
         start.tz_convert(site.timezone), end.tz_convert(site.timezone))
     if obs_df.empty:
         logger.warning(f'Data for site {site.name} contained no '
                        f'entries from {start} to {end}.')
         return pd.DataFrame()
-    obs_df = obs_df.rename(columns=DOE_RTC_VARIABLE_MAP).tz_localize(
+    obs_df = obs_df.rename(columns=DOE_ARM_VARIABLE_MAP).tz_localize(
         site.timezone)
-    # W to MW
-    if 'ac_power' in obs_df:
-        obs_df['ac_power'] = obs_df['ac_power'] / 1e6
     return obs_df
 
 
 def update_observation_data(api, sites, observations, start, end):
-    """Post new observation data to a list of DOE RTC Observations
+    """Post new observation data to a list of DOE ARM Observations
     from start to end.
 
     api : solarforecastarbiter.io.api.APISession
@@ -137,12 +161,18 @@ def update_observation_data(api, sites, observations, start, end):
     end : datetime
         The end of the period to request data for.
     """
-    doe_rtc_api_key = os.getenv('DOE_RTC_API_KEY')
-    if doe_rtc_api_key is None:
-        raise KeyError('"DOE_RTC_API_KEY" environment variable must be '
-                       'set to update DOE RTC observation data.')
-    doe_rtc_sites = common.filter_by_networks(sites, 'DOE RTC')
-    for site in doe_rtc_sites:
+    doe_arm_api_key = os.getenv('DOE_ARM_API_KEY')
+    if doe_arm_api_key is None:
+        raise KeyError('"DOE_ARM_API_KEY" environment variable must be '
+                       'set to update DOE ARM observation data.')
+    doe_arm_user_id = os.getenv('DOE_ARM_USER_ID')
+    if doe_arm_user_id is None:
+        raise KeyError('"DOE_ARM_USER_ID" environment variable must be '
+                       'set to update DOE ARM observation data.')
+
+    doe_arm_sites = common.filter_by_networks(sites, 'DOE ARM')
+    for site in doe_arm_sites:
         common.update_site_observations(
-            api, partial(fetch, doe_rtc_api_key=doe_rtc_api_key), site,
-            observations, start, end)
+            api, partial(fetch, doe_arm_user_id=doe_arm_user_id,
+                         doe_arm_api_key=doe_arm_api_key),
+            site, observations, start, end)
