717573b397e80a096967fa1ff8989d2758a597fe
Tony Lorenzo <atlorenzo@email.arizona.edu>
2020-05-08 15:01:45 -0700

    document render

--- solarforecastarbiter/reports/template.py
+++ solarforecastarbiter/reports/template.py
@@ -193,7 +193,56 @@ def html_to_tex(value):
     return value
 
 
-def render_pdf(report, dash_url, with_timeseries=True, max_runs=5):
+def render_pdf(report, dash_url, max_runs=5):
+    """
+    Create a PDF report using LaTeX.
+
+    Parameters
+    ----------
+    report: :py:class:`solarforecastarbiter.datamodel.Report`
+    dash_url: str
+        URL of the Solar Forecast arbiter dashboard to use when building links.
+    max_runs: int, default 5
+        Maximum number of times to run pdflatex
+
+    Returns
+    -------
+    bytes
+        The rendered PDF report
+
+    Notes
+    -----
+    This code was inspired by the latex package available at
+    https://github.com/mbr/latex/ under the following license:
+
+    Copyright (c) 2015, Marc Brinkmann
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice, this
+      list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above copyright notice,
+      this list of conditions and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
+
+    * Neither the name of latex nor the names of its
+      contributors may be used to endorse or promote products derived from
+      this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+    FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+    DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+    OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+    """  # NOQA
     env = Environment(
         loader=ChoiceLoader([
             PackageLoader('solarforecastarbiter.reports', 'templates/pdf'),
@@ -212,7 +261,7 @@ def render_pdf(report, dash_url, with_timeseries=True, max_runs=5):
         line_comment_prefix='%#'
     )
     env.filters['html_to_tex'] = html_to_tex
-    kwargs = _get_render_kwargs(report, dash_url, with_timeseries)
+    kwargs = _get_render_kwargs(report, dash_url, False)
     with tempfile.TemporaryDirectory() as _tmpdir:
         tmpdir = Path(_tmpdir)
         template = env.get_template('base.tex')
