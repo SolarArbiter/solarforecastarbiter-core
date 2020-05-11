b395c039a495683703d95ccbb29984f6cc210465
Will Holmgren <william.holmgren@gmail.com>
2020-05-11 10:08:37 -0700

    fix modeling parameters bug, fix mw bug

--- solarforecastarbiter/io/reference_observations/pvdaq.py
+++ solarforecastarbiter/io/reference_observations/pvdaq.py
@@ -43,12 +43,12 @@ def adjust_site_parameters(site):
     """
     with open(DEFAULT_SITEFILE) as fp:
         sites_metadata = json.load(fp)['sites']
-    site_api_id = site['extra_parameters']['network_api_id']
+    site_api_id = int(site['extra_parameters']['network_api_id'])
     for site_metadata in sites_metadata:
         site_extra_params = json.loads(site_metadata['extra_parameters'])
         if site_extra_params['network_api_id'] == site_api_id:
             site_out = site.copy()
-            site_out['modeling_parameters'] = sites_metadata[
+            site_out['modeling_parameters'] = site_metadata[
                 'modeling_parameters']
             site_out['extra_parameters'].update(site_extra_params)
             return site_out
