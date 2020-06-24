a9848741fdec5035244a89a6f4e024f26acbccad
David P. Larson <dlarson@epri.com>
2020-06-24 11:45:47 -0700

    reference_data: add EIA

--- solarforecastarbiter/io/reference_observations/reference_data.py
+++ solarforecastarbiter/io/reference_observations/reference_data.py
@@ -38,7 +38,8 @@ from solarforecastarbiter.io.reference_observations import (
     rtc,
     common,
     arm,
-    pvdaq
+    pvdaq,
+    eia
 )
 
 
@@ -51,12 +52,14 @@ NETWORKHANDLER_MAP = {
     'NREL MIDC': midc,
     'DOE RTC': rtc,
     'DOE ARM': arm,
-    'NREL PVDAQ': pvdaq
+    'NREL PVDAQ': pvdaq,
+    'EIA Open Data': eia,
 }
 
 # list of options for the 'network' argument
 NETWORK_OPTIONS = ['NOAA SURFRAD', 'NOAA SOLRAD', 'NOAA USCRN', 'NREL MIDC',
-                   'UO SRML', 'DOE RTC', 'DOE ARM', 'NREL PVDAQ']
+                   'UO SRML', 'DOE RTC', 'DOE ARM', 'NREL PVDAQ',
+                   'EIA Open Data']
 
 DEFAULT_SITEFILE = resource_filename(
     Requirement.parse('solarforecastarbiter'),
@@ -94,6 +97,9 @@ https://www.arm.gov/
 
 NREL PVDAQ: National Renewable Energy Laboratory PV Data Acquisition
 https://developer.nrel.gov/docs/solar/pvdaq-v3/
+
+EIA Open Data: U.S. Energy Information Administration Open Data
+https://www.eia.gov/opendata/
 """  # noqa: E501
 
 
