cfe594c3816d710565d830653ee13747448eed2b
David P. Larson <dlarson@epri.com>
2020-06-24 17:15:49 -0700

    io/reference_observations/eia: add more tests

---
+++ solarforecastarbiter/io/reference_observations/tests/test_eia.py
@@ -0,0 +1,101 @@
+import pytest
+import re
+import pandas as pd
+
+from solarforecastarbiter.datamodel import Site
+from solarforecastarbiter.io import api
+from solarforecastarbiter.io.reference_observations import eia
+
+
+@pytest.fixture
+def session(requests_mock):
+    return api.APISession('')
+
+
+@pytest.fixture
+def site():
+    return Site(
+        name='CAISO',
+        latitude=37.0,
+        longitude=-120.0,
+        elevation=0.0,
+        timezone='Etc/GMT+7',
+        site_id='',
+        provider='',
+        extra_parameters='{"network_api_id": "CISO-ALL", "attribution": "https://www.eia.gov/opendata/", "network": "EIA", "network_api_abbreviation": "eia", "observation_interval_length": 1}',  # noqa: E501
+    )
+
+
+@pytest.fixture
+def log(mocker):
+    log = mocker.patch('solarforecastarbiter.io.reference_observations.'
+                       'pvdaq.logger')
+    return log
+
+
+@pytest.fixture()
+def mock_list_sites(mocker, many_sites):
+    mocker.patch('solarforecastarbiter.io.api.APISession.list_sites',
+                 return_value=many_sites)
+
+
+def test_initialize_site_observations(
+        requests_mock, mocker, session, site, single_observation,
+        single_observation_text, mock_list_sites):
+    matcher = re.compile(f'{session.base_url}/observations/.*')
+    requests_mock.register_uri('POST', matcher,
+                               text=single_observation.observation_id)
+    requests_mock.register_uri('GET', matcher, content=single_observation_text)
+    status = mocker.patch(
+        'solarforecastarbiter.io.api.APISession.create_observation')
+    eia.initialize_site_observations(session, site)
+    assert status.called
+
+
+def test_fetch(mocker, session, site):
+    status = mocker.patch(
+        'solarforecastarbiter.io.fetch.eia.get_eia_data'
+    )
+    start = pd.Timestamp('2020-01-01T0000Z')
+    end = pd.Timestamp('2020-01-02T0000Z')
+    api_key = 'bananasmoothie'
+    eia.fetch(session, site, start, end, eia_api_key=api_key)
+    assert status.called
+
+
+def test_initialize_site_forecasts(
+        requests_mock, mocker, session, site, single_forecast,
+        single_forecast_text, mock_list_sites):
+    matcher = re.compile(f'{session.base_url}/forecasts/.*')
+    requests_mock.register_uri('POST', matcher,
+                               text=single_forecast.forecast_id)
+    requests_mock.register_uri('GET', matcher, content=single_forecast_text)
+    status = mocker.patch(
+        'solarforecastarbiter.io.reference_observations.common.'
+        'create_forecasts')
+    eia.initialize_site_forecasts(session, site)
+    assert status.called
+
+
+@pytest.fixture
+def mock_eia_creds(mocker):
+    mocker.patch.dict('os.environ', {'EIA_API_KEY': 'fake_key'})
+
+
+def test_update_observation_data(mocker, session, site, mock_eia_creds):
+    obs_update = mocker.patch(
+        'solarforecastarbiter.io.reference_observations.common.'
+        'update_site_observations')
+    start = pd.Timestamp('20200101T0000Z')
+    end = pd.Timestamp('20200102T0000Z')
+    eia.update_observation_data(session, [site], [], start, end)
+    obs_update.assert_called()
+    assert obs_update.call_count == 1
+
+
+def test_update_observation_data_no_creds(session, site):
+    start = pd.Timestamp('20200101T0000Z')
+    end = pd.Timestamp('20200102T0000Z')
+    with pytest.raises(KeyError) as e:
+        eia.update_observation_data(session, [site], [], start, end)
+    assert 'environment variable' in str(e.value)
