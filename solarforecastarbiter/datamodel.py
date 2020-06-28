ce3920ec6aec661d7e7e5ecf1b7f1f63fb64f956
Tony Lorenzo <atlorenzo@email.arizona.edu>
2020-06-28 06:54:24 -0700

    Update solarforecastarbiter/datamodel.py
    Co-authored-by: Will Holmgren <william.holmgren@gmail.com>

--- solarforecastarbiter/datamodel.py
+++ solarforecastarbiter/datamodel.py
@@ -760,7 +760,7 @@ class EventForecast(Forecast):
         forecast runs issued multiple times within one day (e.g. hourly),
         this specifies the first issue time of day. Additional issue times
         are uniquely determined by the first issue time and the run length &
-        issue frequency attribute.  This is assumed to be a UTC time.
+        issue frequency attribute. This is assumed to be a UTC time.
     lead_time_to_start : pandas.Timedelta
         The difference between the issue time and the start of the first
         forecast interval, e.g. 1 hour.
