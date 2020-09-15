b39a6bd96035ed26a63a754282fde6b8bd3a8905
Leland Boeman <lboeman@email.arizona.edu>
2020-09-15 10:51:23 -0700

    Irradiance data at arm sites (#536)
    * update reference sites to remove datastreams and use location ids
    
    * remove duplicate maple city, lamont C1 site that has stopped reporting
    
    * add datastreams with date ranges to extra params
    
    * change irrad to qcrad
    
    * add retry handling to catch chunkedencoding errors
    
    * handle datastreams in extra parameters and overlapping stream data
    
    * tests
    
    * flip json declaration datastreams to match usage in code to avoid confusing flipping
    
    * fix test for flip
    
    * fix fetch test
    
    * dont rename lamont to avoid renaming of all obs/fx with varying use of site name
    
    * set conftest id not to match real id
    
    * flake8
    
    * coverage
    
    * further test coverage
    
    * Update solarforecastarbiter/io/fetch/arm.py
    
    Co-authored-by: Tony Lorenzo <atlorenzo@email.arizona.edu>
    
    * Update solarforecastarbiter/io/fetch/arm.py
    
    Co-authored-by: Tony Lorenzo <atlorenzo@email.arizona.edu>
    
    * Update solarforecastarbiter/io/fetch/arm.py
    
    Co-authored-by: Tony Lorenzo <atlorenzo@email.arizona.edu>
    
    * Update solarforecastarbiter/io/reference_observations/tests/test_ref_arm.py
    
    Co-authored-by: Tony Lorenzo <atlorenzo@email.arizona.edu>
    
    * update comments, remove overlap code in favor of configuring without overlap
    
    * fix error message in test
    
    * add whatsnew note
    
    * fix overlapping stream ranges
    
    * throw error on stream overlap
    
    * update whatsnew with note on breaking change
    
    Co-authored-by: Tony Lorenzo <atlorenzo@email.arizona.edu>

--- solarforecastarbiter/io/reference_observations/arm.py
+++ solarforecastarbiter/io/reference_observations/arm.py
@@ -1,6 +1,8 @@
 from functools import partial
+import json
 import logging
 import os
+from pkg_resources import resource_filename, Requirement
 
 
 import pandas as pd
@@ -12,11 +14,11 @@ from solarforecastarbiter.io.reference_observations import (
     common, default_forecasts)
 
 
-logger = logging.getLogger('reference_data')
+DEFAULT_SITEFILE = resource_filename(
+    Requirement.parse('solarforecastarbiter'),
+    'solarforecastarbiter/io/reference_observations/'
+    'arm_reference_sites.json')
 
-
-# ARM data streams include 'met' for meteorological sites and 'qcrad' for
-# irradiance data.
 DOE_ARM_SITE_VARIABLES = {
     'qcrad': arm.IRRAD_VARIABLES,
     'met': arm.MET_VARIABLES,
@@ -31,15 +33,17 @@ DOE_ARM_VARIABLE_MAP = {
     'wspd_arith_mean': 'wind_speed',
 }
 
+logger = logging.getLogger('reference_data')
 
-def _determine_site_vars(datastream):
+
+def _determine_stream_vars(datastream):
     """Returns a list of variables available based on datastream name.
 
     Parameters
     ----------
     datastream: str
-        datastream field of the site. This should be found in the
-        `extra_parameters` field as `network_api_id`
+        Datastream name, or the product name. This string is searched for
+        `met` or `qcrad` and returns a list of expected variables.
 
     Returns
     -------
@@ -70,9 +74,9 @@ def initialize_site_observations(api, site):
         logger.error(f'Failed to initialize observations  for {site.name} '
                      'extra parameters could not be loaded.')
         return
-    site_arm_vars = _determine_site_vars(site_extra_params['network_api_id'])
-    site_sfa_vars = [DOE_ARM_VARIABLE_MAP[v] for v in site_arm_vars]
-    for sfa_var in site_sfa_vars:
+
+    site_vars = site_variables_from_extra_params(site_extra_params)
+    for sfa_var in site_vars:
         logger.info(f'Creating {sfa_var} at {site.name}')
         try:
             common.create_observation(
@@ -100,9 +104,10 @@ def initialize_site_forecasts(api, site):
         logger.error('Failed to initialize reference forecasts for '
                      f'{site.name} extra parameters could not be loaded.')
         return
-    site_arm_vars = _determine_site_vars(site_extra_params['network_api_id'])
-    sfa_vars = [DOE_ARM_VARIABLE_MAP[v] for v in site_arm_vars]
-    common.create_forecasts(api, site, sfa_vars,
+
+    site_vars = site_variables_from_extra_params(site_extra_params)
+
+    common.create_forecasts(api, site, site_vars,
                             default_forecasts.TEMPLATE_FORECASTS)
 
 
@@ -133,17 +138,67 @@ def fetch(api, site, start, end, *, doe_arm_user_id, doe_arm_api_key):
         site_extra_params = common.decode_extra_parameters(site)
     except ValueError:
         return pd.DataFrame()
-    doe_arm_datastream = site_extra_params['network_api_id']
-    site_vars = _determine_site_vars(doe_arm_datastream)
-    obs_df = arm.fetch_arm(
-        doe_arm_user_id, doe_arm_api_key, doe_arm_datastream, site_vars,
-        start.tz_convert(site.timezone), end.tz_convert(site.timezone))
-    if obs_df.empty:
-        logger.warning(f'Data for site {site.name} contained no '
-                       f'entries from {start} to {end}.')
+
+    available_datastreams = site_extra_params['datastreams']
+
+    datastreams = {}
+    # Build a dict with top-level keys to 'met' and 'qcrad' if meteorological
+    # or irradiance  data exists at the site. This is to later group dataframes
+    # created from each datastream by the type of data found in the stream.
+    for ds_type in ['met', 'qcrad']:
+        if ds_type in available_datastreams:
+            ds_type_dict = {}
+            streams = available_datastreams[ds_type]
+
+            # When a dict is present each key is a datastream and value is
+            # a date range for which the datastream contains data. We need to
+            # determine which streams to use to get all of the requested data.
+            if isinstance(streams, dict):
+                ds_type_dict.update(
+                    find_stream_data_availability(streams, start, end))
+            else:
+                # If a single string datastream name exists, we assume that all
+                # available data is contained in the stream. Deferring to the
+                # data fetch process, which will fail to retrieve data and
+                # continue gracefully.
+                ds_type_dict[streams] = (start, end)
+            datastreams[ds_type] = ds_type_dict
+
+    site_dfs = []
+
+    for stream_type in datastreams:
+        # Stitch together all the datastreams with similar data.
+        stream_type_dfs = []
+        for datastream, date_range in datastreams[stream_type].items():
+            stream_df = arm.fetch_arm(
+                doe_arm_user_id,
+                doe_arm_api_key,
+                datastream,
+                _determine_stream_vars(datastream),
+                date_range[0].tz_convert(site.timezone),
+                date_range[1].tz_convert(site.timezone)
+            )
+            if stream_df.empty:
+                logger.warning(f'Datastream {datastream} for site {site.name} '
+                               f'contained no entries from {start} to {end}.')
+            else:
+                stream_type_dfs.append(stream_df)
+        if stream_type_dfs:
+            # Concatenate all dataframes of similar data
+            stream_type_df = pd.concat(stream_type_dfs)
+            site_dfs.append(stream_type_df)
+
+    if site_dfs:
+        # Join dataframes with different variables along the index, this has
+        # the side effect of introducing missing data if any requests have
+        # failed.
+        obs_df = pd.concat(site_dfs, axis=1)
+        obs_df = obs_df.rename(columns=DOE_ARM_VARIABLE_MAP)
+        return obs_df
+    else:
+        logger.warning(f'Data for site {site.name} contained no entries from '
+                       f'{start} to {end}.')
         return pd.DataFrame()
-    obs_df = obs_df.rename(columns=DOE_ARM_VARIABLE_MAP)
-    return obs_df
 
 
 def update_observation_data(api, sites, observations, start, end):
@@ -176,3 +231,172 @@ def update_observation_data(api, sites, observations, start, end):
             api, partial(fetch, doe_arm_user_id=doe_arm_user_id,
                          doe_arm_api_key=doe_arm_api_key),
             site, observations, start, end)
+
+
+def adjust_site_parameters(site):
+    """Updates extra parameters with applicable datastreams from
+    `arm_reference_sites.json`
+
+    Parameters
+    ----------
+    site: dict
+
+    Returns
+    -------
+    dict
+        Copy of input with updated extra parameters.
+    """
+    with open(DEFAULT_SITEFILE) as fp:
+        sites_metadata = json.load(fp)['sites']
+
+    # ARM has multiple 'locations' at each 'site'. For example, the Southern
+    # Great Plains (SGP) ARM site has many locations throughout Oklahoma, and
+    # neighboring states. Each location is identified by a code, e.g. Byron
+    # Oklahoma is location `E11` at the SGP site, and it's data is accessed via
+    # datastreams with the pattern `sgp<product>e11.<data-level>` where product
+    # indicates the contents of the datastream (we are interested in `met` and
+    # `qcrad1long` products) and data-level indicates quality and any
+    # processing applied to the data. In the Solar Forecast Arbiter we store
+    # each 'location' as a SFA site. We use the `network_api_id` to indicate
+    # the ARM site's location code and `network_api_abbreviation` to represent
+    # ARM "site code (e.g. `sgp`). Both of these keys must be used to  identify
+    # a site in the Solar Forecast Arbiter, because ARM's location ids are only
+    # unique for a given ARM site.
+    arm_location_id = site['extra_parameters']['network_api_id']
+    arm_site_id = site['extra_parameters']['network_api_abbreviation']
+
+    for site_metadata in sites_metadata:
+        site_extra_params = json.loads(site_metadata['extra_parameters'])
+        if (
+            site_extra_params['network_api_id'] == arm_location_id
+            and site_extra_params['network_api_abbreviation'] == arm_site_id
+
+        ):
+            site_out = site.copy()
+            site_out['extra_parameters'] = site_extra_params
+            return site_out
+    return site
+
+
+def find_stream_data_availability(streams, start, end):
+    """Determines what date ranges to use for each datastream. Date ranges of
+    each stream must not overlap.
+
+    Parameters
+    ----------
+    streams: dict
+        Dict where values are string datastream names and values are iso8601
+        date ranges `start/end` indicating the period of data available at
+        that datastream.
+    start: datetime
+        The start of the period to request data for.
+    end: datetime
+        The end of the period to request data for.
+
+    Returns
+    -------
+    dict
+        Dict where keys are datastreams and values are two element lists of
+        `[start datetime, end datetime]` that when considered together should
+        span all of the available data between the requested start and end.
+
+    Raises
+    ------
+    ValueError
+        If streams in the dictionary have overlapping ranges.
+    """
+    stream_range_dict = {}
+    streams_with_ranges = [(stream, parse_iso_date_range(date_range))
+                           for stream, date_range in streams.items()]
+    streams_overlap = detect_stream_overlap(streams_with_ranges)
+    if streams_overlap:
+        raise ValueError(f'Overlapping datastreams found in {streams.keys()}.')
+
+    # Find the overlap between each streams available data, and the requested
+    # period
+    for datastream, stream_range in streams_with_ranges:
+        overlap = get_period_overlap(
+            start, end, stream_range[0], stream_range[1])
+        if overlap is None:
+            # The datastream did not contain any data within the requested
+            # range, we don't need to use it for this request.
+            continue
+        else:
+            stream_range_dict[datastream] = overlap
+    return stream_range_dict
+
+
+def get_period_overlap(request_start, request_end, avail_start, avail_end):
+    """Finds period of overlap between the requested time range and the
+    available period.
+
+    Parameters
+    ----------
+    request_start: datetime-like
+        Start of the period of requested data.
+    request_end: datetime-like
+        End of the period of requested data.
+    avail_start: datetime-like
+        Start of the available data.
+    avail_end: datatime-like
+        End of available data.
+
+    Returns
+    -------
+    start, end: list of datetime or None
+        Start and end of overlapping period, or None if no overlap occurred.
+    """
+    if request_start < avail_end and request_end > avail_start:
+        if request_start < avail_start:
+            start = avail_start
+        else:
+            start = request_start
+        if request_end > avail_end:
+            end = avail_end
+        else:
+            end = request_end
+        return [start, end]
+    else:
+        return None
+
+
+def parse_iso_date_range(date_range_string):
+    """Parses a date range string in iso8601 format ("start date/end date")
+    into a tuple of pandas timestamps `(start, end)`.
+    """
+    start, end = date_range_string.split('/')
+    return (pd.Timestamp(start, tz='utc'), pd.Timestamp(end, tz='utc'))
+
+
+def site_variables_from_extra_params(site_extra_params):
+    """Return variables expected at the site, based on the content of the
+    `datastreams` attribute of the site's extra parameters.
+    """
+    return [DOE_ARM_VARIABLE_MAP[arm_var]
+            for stream_type in site_extra_params['datastreams'].keys()
+            for arm_var in _determine_stream_vars(stream_type)]
+
+
+def detect_stream_overlap(stream_list):
+    """Detects if a list of streams contain any overlap
+
+    Parameters
+    ----------
+    stream_list: list of 3-tuples
+        List of (datastream, (start, end)) where datastream is a string, start
+        and end are pandas timestamps.
+
+    Returns
+    -------
+    bool
+        Returns True if any of the stream's ranges overlap,  otherwise False.
+    """
+    overlap = []
+    for i, stream in enumerate(stream_list):
+        date_range = stream[1]
+        for j, other_stream in enumerate(stream_list[:i] + stream_list[i+1:]):
+            other_date_range = other_stream[1]
+            overlap.append(get_period_overlap(
+                date_range[0], date_range[1],
+                other_date_range[0], other_date_range[1]))
+    return any(overlap)
--- solarforecastarbiter/io/reference_observations/tests/test_ref_arm.py
+++ solarforecastarbiter/io/reference_observations/tests/test_ref_arm.py
@@ -20,8 +20,8 @@ def log(mocker):
     ('sgpmet', arm.DOE_ARM_SITE_VARIABLES['met']),
     ('badstream', []),
 ])
-def test__detemrine_site_vars(ds, expected):
-    assert arm._determine_site_vars(ds) == expected
+def test__determine_stream_vars(ds, expected):
+    assert arm._determine_stream_vars(ds) == expected
 
 
 @pytest.fixture
@@ -46,8 +46,8 @@ def test_initialize_site_obs(mock_api, mock_obs_creation):
 def test_initialize_site_obs_http_error(
         log, mock_api, mock_obs_creation_error):
     arm.initialize_site_observations(mock_api, site_objects[0])
-    assert log.error.call_count == 3
-    assert log.debug.call_count == 3
+    assert log.error.call_count == 6
+    assert log.debug.call_count == 6
 
 
 @pytest.fixture
@@ -93,3 +93,207 @@ def test_fetch(mocker, mock_api):
                      doe_arm_user_id='id', doe_arm_api_key='key')
     other_fetch.assert_called()
     assert data.empty
+
+
+mock_data = [[{'timestamp': '2020-01-01T0000Z', 'a': 5}],
+             [{'timestamp': '2020-01-01T0001Z', 'a': 6}],
+             [{'timestamp': '2020-01-01T0000Z', 'b': 5},
+              {'timestamp': '2020-01-01T0001Z', 'b': 6}]]
+
+
+def test_fetch_mocked_data(mocker, mock_api):
+    start = pd.Timestamp('20191231T0000Z')
+    end = pd.Timestamp('20200102T0000Z')
+    return_data = (d for d in mock_data)
+    other_fetch = mocker.patch(
+        'solarforecastarbiter.io.fetch.arm.fetch_arm')
+
+    def set_return(*args, **kwargs):
+        df_data = next(return_data)
+        return pd.DataFrame.from_records(df_data, index='timestamp')
+
+    other_fetch.side_effect = set_return
+
+    data = arm.fetch(mock_api, site_objects[0], start, end,
+                     doe_arm_user_id='id', doe_arm_api_key='key')
+    other_fetch.assert_called()
+    expected = pd.DataFrame.from_records(
+        [{'timestamp': '2020-01-01T0000Z', 'a': 5, 'b': 5},
+         {'timestamp': '2020-01-01T0001Z', 'a': 6, 'b': 6}],
+        index='timestamp')
+    pd.testing.assert_frame_equal(data, expected)
+
+
+def test_fetch_param_valueerror(mocker, mock_api):
+    decode_ep = mocker.patch(
+        'solarforecastarbiter.io.reference_observations.arm.common'
+        '.decode_extra_parameters')
+    decode_ep.side_effect = ValueError
+    start = pd.Timestamp('20191231T0000Z')
+    end = pd.Timestamp('20200102T0000Z')
+    data = arm.fetch(mock_api, site_objects[0], start, end,
+                     doe_arm_user_id='id', doe_arm_api_key='key')
+    assert data.empty
+
+
+@pytest.mark.parametrize('reqstart,reqend,availstart,availend,estart,eend', [
+    ('2020-01-01', '2020-02-01',
+     '2019-01-01', '2021-02-01',
+     '2020-01-01', '2020-02-01'),
+    ('2020-01-15', '2020-01-18',
+     '2020-01-01', '2020-02-01',
+     '2020-01-15', '2020-01-18'),
+    ('2020-01-15', '2020-03-18',
+     '2020-01-01', '2020-02-01',
+     '2020-01-15', '2020-02-01'),
+    ('2019-01-15', '2020-03-18',
+     '2020-01-01', '2020-02-01',
+     '2020-01-01', '2020-02-01'),
+    ('2019-01-15', '2020-01-18',
+     '2020-01-01', '2020-02-01',
+     '2020-01-01', '2020-01-18'),
+])
+def test_get_period_overlap(
+        reqstart, reqend, availstart, availend, estart, eend):
+    start, end = arm.get_period_overlap(
+        pd.Timestamp(reqstart),
+        pd.Timestamp(reqend),
+        pd.Timestamp(availstart),
+        pd.Timestamp(availend),
+    )
+    assert start == pd.Timestamp(estart)
+    assert end == pd.Timestamp(eend)
+
+
+def test_get_period_overlap_no_overlap():
+    overlap = arm.get_period_overlap(
+        pd.Timestamp('2020-01-01', tz='utc'),
+        pd.Timestamp('2020-02-01', tz='utc'),
+        pd.Timestamp('2020-02-02', tz='utc'),
+        pd.Timestamp('2020-03-01', tz='utc'),
+    )
+    assert overlap is None
+
+
+@pytest.mark.parametrize('range_string,exstart,exend', [
+    ('2019-01-01/2021-01-01',
+     pd.Timestamp('2019-01-01', tz='utc'),
+     pd.Timestamp('2021-01-01', tz='utc')),
+    ('1994-11-13/2020-01-01',
+     pd.Timestamp('1994-11-13', tz='utc'),
+     pd.Timestamp('2020-01-01', tz='utc')),
+    ])
+def test_parse_iso_date_range(range_string, exstart, exend):
+    start, end = arm.parse_iso_date_range(range_string)
+    assert start == exstart
+    assert end == exend
+
+
+def test_parse_iso_date_range_now():
+    start, end = arm.parse_iso_date_range('2019-01-01/now')
+    assert start == pd.Timestamp('2019-01-01', tz='utc')
+    expected_end = pd.Timestamp.utcnow()
+    assert end.month == expected_end.month
+    assert end.day == expected_end.day
+    assert end.year == expected_end.year
+    assert end.hour == expected_end.hour
+
+
+@pytest.mark.parametrize('stream_dict,start,end,expected', [
+    ({'stream1': '2019-12-01/2020-02-01',
+      'stream2': '2019-01-01/2019-12-01'},
+     pd.Timestamp('2019-01-01', tz='utc'),
+     pd.Timestamp('2020-02-01', tz='utc'),
+     {'stream1': [pd.Timestamp('2019-12-01', tz='utc'),
+                  pd.Timestamp('2020-02-01', tz='utc')],
+      'stream2': [pd.Timestamp('2019-01-01', tz='utc'),
+                  pd.Timestamp('2019-12-01', tz='utc')]}),
+    ({'stream2': '2019-01-01/2019-12-01',
+      'stream1': '2019-12-01/2020-02-01'},
+     pd.Timestamp('2019-12-12', tz='utc'),
+     pd.Timestamp('2020-02-01', tz='utc'),
+     {'stream1': [pd.Timestamp('2019-12-12', tz='utc'),
+                  pd.Timestamp('2020-02-01', tz='utc')]}),
+    ({'stream2': '2019-01-01/2019-12-01',
+      'stream1': '2019-12-01/2020-02-01'},
+     pd.Timestamp('2019-01-01', tz='utc'),
+     pd.Timestamp('2019-03-01', tz='utc'),
+     {'stream2': [pd.Timestamp('2019-01-01', tz='utc'),
+                  pd.Timestamp('2019-03-01', tz='utc')]}),
+])
+def test_find_stream_data_availability(stream_dict, start, end, expected):
+    available_stream_dict = arm.find_stream_data_availability(
+        stream_dict, start, end)
+    assert available_stream_dict == expected
+
+
+@pytest.mark.parametrize('stream_dict', [
+    ({'stream1': '2019-12-01/2020-02-01',
+      'stream2': '2019-01-01/2020-01-01'}),
+    ({'stream1': '2019-12-01/2020-02-01',
+      'stream2': '2019-01-01/2020-01-01',
+      'stream3': '2020-01-20/2020-03-01'}),
+])
+def test_find_stream_data_availability_overlap(stream_dict):
+    with pytest.raises(ValueError):
+        arm.find_stream_data_availability(
+            stream_dict,
+            pd.Timestamp('2019-12-12', tz='utc'),
+            pd.Timestamp('2020-02-01', tz='utc'))
+
+
+@pytest.mark.parametrize('site,expected_params', [
+    ({'extra_parameters': {
+         'network_api_id': 'E11',
+         'network_api_abbreviation': 'sgp'}
+      },
+     {'extra_parameters': {
+         "network_api_abbreviation": "sgp",
+         "attribution": "https://www.arm.gov/capabilities/vaps/qcrad",
+         "network": "DOE ARM",
+         "arm_site_id": "sgp",
+         "network_api_id": "E11",
+         "observation_interval_length": 1.0,
+         "datastreams": {
+             "met": "sgpmetE11.b1",
+             "qcrad": {
+                 "sgpqcrad1longE11.c2": "1995-06-30/2019-08-02",
+                 "sgpqcrad1longE11.c1": "2019-08-02/now"
+                }
+            }
+         }
+      }), ({
+         'extra_parameters': {
+            'network_api_id': 'dne',
+            'network_api_abbreviation': 'nsa'}
+      }, {
+        'extra_parameters': {
+            'network_api_id': 'dne',
+            'network_api_abbreviation': 'nsa'}
+      })
+])
+def test_adjust_site_parameters(site, expected_params):
+    adjusted = arm.adjust_site_parameters(site)
+    adjusted_params = adjusted['extra_parameters']
+    for k, v in adjusted_params.items():
+        assert adjusted_params[k] == expected_params['extra_parameters'][k]
+
+
+@pytest.mark.parametrize('stream_list,expected', [
+    ([('stream1', (pd.Timestamp('2019-12-01'), pd.Timestamp('2020-02-01'))),
+      ('stream2', (pd.Timestamp('2019-01-01'), pd.Timestamp('2019-12-01')))],
+     False),
+    ([('stream1', (pd.Timestamp('2019-12-01'), pd.Timestamp('2020-02-01'))),
+      ('stream2', (pd.Timestamp('2019-01-01'), pd.Timestamp('2019-12-15')))],
+     True),
+    ([('stream1', (pd.Timestamp('2019-12-01'), pd.Timestamp('2020-02-01'))),
+      ('stream2', (pd.Timestamp('2019-01-01'), pd.Timestamp('2019-12-01'))),
+      ('stream3', (pd.Timestamp('2020-02-01'), pd.Timestamp('2020-12-01')))],
+     False),
+    ([('stream1', (pd.Timestamp('2019-12-01'), pd.Timestamp('2020-02-01'))),
+      ('stream2', (pd.Timestamp('2019-01-01'), pd.Timestamp('2019-12-15'))),
+      ('stream3', (pd.Timestamp('2020-02-01'), pd.Timestamp('2020-12-01')))],
+     True),
+])
+def test_detect_stream_overlap(stream_list, expected):
+    assert arm.detect_stream_overlap(stream_list) == expected
