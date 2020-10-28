fb6b27f10245aeef40a953d077b6358c0e2f1a8c
Tony Lorenzo <atlorenzo@email.arizona.edu>
2020-10-28 09:20:09 -0700

    use nan* equivalent summary stats

--- solarforecastarbiter/metrics/summary.py
+++ solarforecastarbiter/metrics/summary.py
@@ -5,12 +5,12 @@ import numpy as np
 
 # Add new metrics to this map to map shorthand to function
 _MAP = {
-    'mean': (np.mean, 'Mean'),
-    'min': (np.min, 'Min'),
-    'max': (np.max, 'Max'),
-    'std': (np.std, 'Std'),
-    'median': (np.median, 'Median'),
-    'var': (np.var, 'Variance'),
+    'mean': (np.nanmean, 'Mean'),
+    'min': (np.nanmin, 'Min'),
+    'max': (np.nanmax, 'Max'),
+    'std': (np.nanstd, 'Std'),
+    'median': (np.nanmedian, 'Median'),
+    'var': (np.nanvar, 'Variance'),
 }
 
 __all__ = [m[0].__name__ for m in _MAP.values()]
