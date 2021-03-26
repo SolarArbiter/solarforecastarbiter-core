033211ea36d3b6e7dcac21e384eab64195664a9c
Will Holmgren <william.holmgren@gmail.com>
2021-03-26 12:13:34 -0700

    remove unused import

--- solarforecastarbiter/metrics/tests/test_preprocessing.py
+++ solarforecastarbiter/metrics/tests/test_preprocessing.py
@@ -4,8 +4,7 @@ import numpy as np
 import pandas as pd
 import pytest
 
-from pandas.testing import (
-    assert_frame_equal, assert_index_equal, assert_series_equal)
+from pandas.testing import assert_index_equal, assert_series_equal
 
 from solarforecastarbiter import datamodel
 from solarforecastarbiter.metrics import preprocessing
