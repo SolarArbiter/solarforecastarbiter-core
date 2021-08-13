f359b715ef8b2dbef413296946c21b52433ee938
Leland Boeman <lboeman@email.arizona.edu>
2021-08-13 14:40:42 -0700

    try aws

--- solarforecastarbiter/io/fetch/nwp.py
+++ solarforecastarbiter/io/fetch/nwp.py
@@ -57,6 +57,12 @@ logger = logging.getLogger(__name__)
 CHECK_URL = 'https://nomads.ncep.noaa.gov/pub/data/nccf/com/{}/prod'
 BASE_URL = 'https://nomads.ncep.noaa.gov/cgi-bin/'
 
+GEFS_BASE_URL = 'https://noaa-gefs-pds.s3.amazonaws.com'
+
+# When querying aws for directories, start-after is used to paginate.
+# 2020-09-23 is the date the expected folder structure with the atmos
+# dir appears.
+GEFS_STARTAFTER = 'gefs.20200923'
 
 GFS_0P25_1HR = {'endpoint': 'filter_gfs_0p25_1hr.pl',
                 'file': 'gfs.t{init_hr:02d}z.pgrb2.0p25.f{valid_hr:03d}',
@@ -288,7 +294,10 @@ async def get_available_dirs(session, model):
     """Get the available date/date+init_hr directories"""
     simple_model = _simple_model(model)
     is_init_date = 'init_date' in model['dir']
-    model_url = BASE_URL + model['endpoint']
+    if simple_model == 'gefs':
+        return await get_available_gefs_dirs(session)
+    else:
+        model_url = BASE_URL + model['endpoint']
 
     async def _get(model_url):
         async with session.get(model_url, raise_for_status=True) as r:
@@ -302,6 +311,32 @@ async def get_available_dirs(session, model):
         list_avail_days = set(
             re.findall(simple_model + '\\.([0-9]{10})', page))
     return list_avail_days
+
+
+@abort_all_on_exception
+async def get_available_gefs_dirs(session, start_after=GEFS_STARTAFTER):
+    params = {
+        'list-type': 2,
+        'delimiter': '/',
+    }
+    if start_after is not None:
+        params['start-after'] = start_after
+    async def _get(session):
+        async with session.get(
+            GEFS_BASE_URL,
+            params=params,
+            raise_for_status=True
+        ) as r:
+            return await r.text()
+    listing = await _get(session)
+    all_dirs = re.findall("gefs\\.([0-9]{8})", listing)
+    if len(all_dirs) < 1000:
+        return all_dirs
+    else:
+        return all_dirs + await get_available_gefs_dirs(
+            session,
+            'gefs.'+all_dirs[-1]
+        )
 
 
 def _process_params(model, init_time):
@@ -371,9 +406,13 @@ async def files_to_retrieve(session, model, modelpath, init_time):
         if filename.exists():
             yield next_params
             continue
-        next_model_url = (CHECK_URL.format(model.get('check_url_name',
-                                                     simple_model))
-                          + next_params['dir'] + '/' + next_params['file'])
+        if simple_model == 'gefs':
+            next_model_url = (GEFS_BASE_URL + next_params['dir']
+                              + '/' + next_params['file'])
+        else:
+            next_model_url = (CHECK_URL.format(model.get('check_url_name',
+                                                         simple_model))
+                              + next_params['dir'] + '/' + next_params['file'])
         while True:
             # is the next file ready?
             try:
@@ -465,8 +504,11 @@ async def fetch_grib_files(session, params, basepath, init_time, chunksize):
     aiohttp.ClientResponseError
         When the HTTP request fails/returns a status code >= 400
     """
-    endpoint = params.pop('endpoint')
-    url = BASE_URL + endpoint
+    if _simple_model(params) == 'gefs':
+        url = GEFS_BASE_URL + params['dir'] + '/' + params['file']
+    else:
+        endpoint = params.pop('endpoint')
+        url = BASE_URL + endpoint
     filename = get_filename(basepath, init_time, params)
     if filename.exists():
         return filename
